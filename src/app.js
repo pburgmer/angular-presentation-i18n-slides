97d9e33e8adf61882fe3b5ee46154ce6b9dd816b
Philipp Burgmer <burgmer@w11k.de>
2017-05-22 01:56:53 +0200

    add content

--- src/app.js
+++ src/app.js
@@ -10,6 +10,44 @@
       'about-us',
       'overview',
 
+      'intro/i18n',
+      'intro/l10n',
+      'intro/requirements-non-functional',
+      'intro/requirements-functional',
+
+      'features/api/title',
+      'features/api/maps',
+      'features/api/leaflet',
+      'features/api/end',
+
+      'features/format/title',
+      'features/format/intro',
+      'features/format/number',
+      'features/format/date',
+      'features/format/sort',
+
+      'features/text/title',
+      'features/text/id-custom',
+      'features/text/id-generated',
+      'features/text/jit',
+      'features/text/aot',
+      'features/text/demo',
+      'features/text/data-format',
+      'features/text/interpolation',
+      'features/text/pluralization',
+      'features/text/selection',
+      'features/text/error-messages',
+
+      'features/modules/title',
+      'features/modules/languages',
+      'features/modules/features',
+
+      'features/styling/title',
+      'features/styling/fonts',
+      'features/styling/right-to-left',
+      'features/styling/layout',
+
+      'summary',
       'end'
     ];
 
